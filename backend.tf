f6dd985f2e3240d7af8b3c41ffe314047674f72d
Manibellam <mani@IP330.localdomain>
2021-11-06 17:20:45 +0530

    updating provider

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "accesslogs77"
-    key    = "/terraform-backend/mani.tfstate"
+    key    = "terraform-backend/mani.tfstate"
     region = "ap-south-1"
   }
 }
--- provider.tf
+++ provider.tf
@@ -12,3 +12,11 @@ provider "aws" {
   shared_credentials_file = "/home/mani/mani_creds"
 region = "ap-southeast-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "accesslogs77"
+    key    = "terraform-backend/mani.tfstate"
+    region = "ap-south-1"
+  }
+}
